d9e2348a22ffeb008fed0257b1320532fe293f2b
Jonas <fastjoe23@hotmail.com>
2019-02-16 21:54:47 +0100

    Bis Pagination

--- boards/views.py
+++ boards/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import UpdateView, ListView
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.db.models import Count
+from django.utils.decorators import method_decorator
+from django.utils import timezone
 
 from .models import Board, Topic, Post
 from .forms import NewTopicForm, PostForm
 
 # Create your views here.
 
-def home(request):
-    boards = Board.objects.all()
-    return render(request, 'home.html', {'boards' : boards})
+class BoardListView(ListView):
+    model = Board
+    context_object_name = 'boards'
+    template_name = 'home.html'
 
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
@@ -64,3 +68,22 @@ def reply_topic(request, pk, topic_pk):
         form = PostForm()
 
     return render(request, 'reply_topic.html', {'topic' : topic, 'form' : form})
+
+@method_decorator(login_required, name='dispatch')
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('message', )
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_pk'
+    context_object_name = 'post'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.updated_by = self.request.user
+        post.updated_at = timezone.now()
+        post.save()
+        return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(created_by=self.request.user)
